29978d2ff40d5a700b9e48bce8f98ae9253cddfb
Jakub Wodecki <wodecki@atlasgroup.cz>
2023-09-20 14:41:31 +0200

    Added basic detail classes

---
+++ spring-boot-example/src/main/java/cz/cvut/fel/camunda/workshops/developer/details/CarDetail.java
@@ -0,0 +1,10 @@
+package cz.cvut.fel.camunda.workshops.developer.details;
+
+import lombok.Data;
+
+@Data
+public class CarDetail {
+    private String spz;
+    private String carManufacturer;
+    private String carType;
+}
---
+++ spring-boot-example/src/main/java/cz/cvut/fel/camunda/workshops/developer/details/ReservationDetail.java
@@ -0,0 +1,8 @@
+package cz.cvut.fel.camunda.workshops.developer.details;
+
+import lombok.Data;
+
+@Data
+public class ReservationDetail {
+    private String dateTime;
+}
