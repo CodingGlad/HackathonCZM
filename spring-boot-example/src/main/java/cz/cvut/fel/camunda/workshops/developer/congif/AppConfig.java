975d0a79d3f3f917904de4153ffb48d6c24952bd
tobias <letobias@fel.cvut.cz>
2023-09-20 15:26:00 +0200

    Encoded api key to base64

--- spring-boot-example/src/main/java/cz/cvut/fel/camunda/workshops/developer/congif/AppConfig.java
+++ spring-boot-example/src/main/java/cz/cvut/fel/camunda/workshops/developer/congif/AppConfig.java
@@ -3,6 +3,11 @@ package cz.cvut.fel.camunda.workshops.developer.congif;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 @Configuration
 @PropertySource("classpath:secret.properties")
 public class AppConfig {
@@ -10,6 +15,9 @@ public class AppConfig {
     private String apiKey;
 
     public String getApiKey() {
-        return apiKey;
+        byte[] decodedBytes = Base64.getDecoder().decode(apiKey);
+
+        // Convert the decoded bytes to a UTF-8 string
+        return new String(decodedBytes, StandardCharsets.UTF_8);
     }
 }
