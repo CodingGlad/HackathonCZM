112cce0ba9432ebc9c4bacf39280bfc2c4bff621
tobias <letobias@fel.cvut.cz>
2023-09-21 12:46:42 +0200

    Updated PostgresHandler

--- spring-boot-example/src/main/java/cz/cvut/fel/camunda/workshops/developer/handlers/ReservationHandler.java
+++ spring-boot-example/src/main/java/cz/cvut/fel/camunda/workshops/developer/handlers/ReservationHandler.java
@@ -5,9 +5,17 @@ import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
 import org.camunda.bpm.client.task.ExternalTask;
 import org.camunda.bpm.client.task.ExternalTaskHandler;
 import org.camunda.bpm.client.task.ExternalTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -15,15 +23,102 @@ import java.util.Map;
         topicName = "handleReservation"
 )
 public class ReservationHandler implements ExternalTaskHandler {
-
+    @Autowired
+    PostgresHandler postgresHandler;
 
     @Override
     public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
 
         Map<String, Object> inputVariables = externalTask.getAllVariables();
+        log.info(inputVariables.toString());
 
-        log.info("Client name: " + inputVariables.get("jmenoVubec"));
+        String carsInsert = """
+                INSERT INTO Cars (spz, manufacturer, model)
+                VALUES (?, ?, ?)
+                ON CONFLICT (spz)
+                DO NOTHING
+                """;
+        try {
+            PreparedStatement carsPreparedStatement = postgresHandler.getConnection().prepareStatement(carsInsert);
+            carsPreparedStatement.setString(1, (String) inputVariables.get("spz"));
+            carsPreparedStatement.setString(2, (String) inputVariables.get("car_manufacturer"));
+            carsPreparedStatement.setString(3, (String) inputVariables.get("model"));
+            carsPreparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("There was a problem adding your car to out database");
+        }
+
+        String driverInsert = """
+                INSERT INTO Drivers (telephone, name, surname, email)
+                VALUES (?, ?, ?, ?)
+                ON CONFLICT (telephone)
+                DO UPDATE SET email = excluded.email;
+                """;
+        try {
+            PreparedStatement carsPreparedStatement = postgresHandler.getConnection().prepareStatement(driverInsert);
+            carsPreparedStatement.setString(1, (String) inputVariables.get("phone"));
+            carsPreparedStatement.setString(2, (String) inputVariables.get("name"));
+            carsPreparedStatement.setString(3, (String) inputVariables.get("surname"));
+            carsPreparedStatement.setString(4, (String) inputVariables.get("mail"));
+            carsPreparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("There was a problem adding your driver profile to out database");
+        }
+
+        if ("need".equals(inputVariables.get("radio"))) {
+            String tireInsert = """
+                    INSERT INTO Tires (code, spz_of_car, size_of_tires
+                     tire_manufacturer, is_stored, season_type)
+                    VALUES (?, ?, ?, ?, ?, ?)
+                    ON CONFLICT (telephone)
+                    DO UPDATE SET email = excluded.email;
+                    """;
+            try {
+                PreparedStatement tirePreparedStatement = postgresHandler.getConnection().prepareStatement(tireInsert);
+                tirePreparedStatement.setString(1, String.valueOf(UUID.randomUUID()));
+                tirePreparedStatement.setString(2, (String) inputVariables.get("spz"));
+                tirePreparedStatement.setString(3, (String) inputVariables.get("tire_size"));
+                tirePreparedStatement.setString(4, (String) inputVariables.get("tire_manufacturer"));
+                tirePreparedStatement.setBoolean(5, false);
+                tirePreparedStatement.setString(6, (String) inputVariables.get("season"));
+                tirePreparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException("There was a problem adding your tires profile to out database");
+            }
+        }
+
+        String reservationInsert = """
+                INSERT INTO Reservation (number_of_reservation, date_of_reservtion, telephone_driver, spz_of_car)
+                VALUES (?, ?, ?, ?);
+                    """;
+        try {
+            PreparedStatement reservationPreparedStatement = postgresHandler.getConnection().prepareStatement(reservationInsert);
+            reservationPreparedStatement.setString(1, String.valueOf(UUID.randomUUID()));
+            reservationPreparedStatement.setTimestamp(2, Timestamp
+                    .valueOf(convertToSQLTimestamp((String) inputVariables.get("date"))));
+            reservationPreparedStatement.setString(3, (String) inputVariables.get("phone"));
+            reservationPreparedStatement.setString(4, (String) inputVariables.get("spz"));
+        } catch (SQLException e) {
+            throw new RuntimeException("There was a problem adding your reservation profile to out database");
+        }
+
+
+        log.info("successfully created a reservation for " + inputVariables.get("date"));
 
         externalTaskService.complete(externalTask);
+    }
+
+    private String convertToSQLTimestamp (String inputDate) {
+        SimpleDateFormat inputDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        SimpleDateFormat outputDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            // Parse the input string into a Date object
+            Date date = inputDateFormat.parse(inputDate);
+
+            // Format the Date object into the PostgreSQL-compatible timestamp format
+            return outputDateFormat.format(date);
+        } catch (ParseException e) {
+            throw new RuntimeException("There was an issue creating your reservation");
+        }
     }
 }
